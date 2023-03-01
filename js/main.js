1c345651d17a520216761fd900b955e2990cbb3f
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-02-28 21:08:43 -0300

    Button functionality + validation of input values

--- js/main.js
+++ js/main.js
@@ -28,7 +28,68 @@ function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+const btnPlus = document.querySelector('[id="btn-plus"]')
+btnPlus.addEventListener('click', btnPlusClick);
 
+function btnPlusClick(event) {
+    event.preventDefault();
+
+    if (inputNumber1.value > 0 && inputNumber2.value > 0) {
+        const resultBox = parseFloat(inputNumber1.value) + parseFloat(inputNumber2.value);
+        document.getElementById('result-box').innerHTML = resultBox;
+    }
+}
+
+const btnTimes = document.querySelector('[id="btn-times"]');
+btnTimes.addEventListener('click', btnTimesClick);
+
+function btnTimesClick(event) {
+    event.preventDefault();
+
+    if (inputNumber1.value > 0 && inputNumber2.value > 0) {
+        const resultBox = parseFloat(inputNumber1.value) * parseFloat(inputNumber2.value);
+        document.getElementById('result-box').innerHTML = resultBox;
+    }
+}
+
+const btnClear = document.querySelector('[id="btn-clear"]');
+btnClear.addEventListener('click', btnClearClick);
+
+function btnClearClick(event) {
+    event.preventDefault();
+    inputNumber1.value = "";
+    inputNumber1.classList.remove("input-error");
+    inputNumber2.value = "";
+    inputNumber2.classList.remove("input-error");
+    document.getElementById('result-box').innerHTML = 0;
+}
+
+// O código abaixo é para prevenir que seja usado qualquer outro valor que não seja número, mas é permitido usar o ponto para casas decimais.
+// The code below is to prevent the use of any value other than a number, but is allowed to use the dot for decimal places.
+
+const inputInvalid1 = document.querySelector('[name="number1"]');
+inputInvalid1.addEventListener('keypress', handleInputNumber1KeyPress);
+
+function handleInputNumber1KeyPress(event) {    
+    const charCode = event.which ? event.which : event.keyCode;
+    const charTyped = String.fromCharCode(charCode);
+    const regex = /^[0-9.]+$/;
+    if (!regex.test(charTyped)) {
+        event.preventDefault();
+    }
+};
+
+const inputInvalid2 = document.querySelector('[name="number2"]');
+inputInvalid2.addEventListener('keypress', handleInputNumber2KeyPress);
+
+function handleInputNumber2KeyPress(event) {
+    const charCode = event.which ? event.which : event.keyCode;
+    const charTyped = String.fromCharCode(charCode);
+    const regex = /^[0-9.]+$/;
+    if (!regex.test(charTyped)) {
+        event.preventDefault();
+    }
+};
 
 
 
